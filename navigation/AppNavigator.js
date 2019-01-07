709431c162d9206c9757ebf9f6a657f92d95e445
Daniel Nam <dannam83@gmail.com>
2019-01-07 08:52:49 -0500

    setup start tabs

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
+import LoadAppScreen from '../screens/LoadAppScreen';
+import StartTabNavigator from './StartTabNavigator';
 import MainTabNavigator from './MainTabNavigator';
 
-export default createAppContainer(createSwitchNavigator({
+export default createAppContainer(createSwitchNavigator(
+  {
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-  Main: MainTabNavigator,
-}));
+    LoadApp: LoadAppScreen,
+    Start: StartTabNavigator,
+    Main: MainTabNavigator,
+  },
+  {
+    initialRouteName: 'LoadApp',
+  }
+));
---
+++ screens/LoadAppScreen.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class LoadAppScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>LoadAppScreen</Text>
+        <Text>LoadAppScreen</Text>
+        <Text>LoadAppScreen</Text>
+        <Text>LoadAppScreen</Text>
+        <Text>LoadAppScreen</Text>
+      </View>
+    );
+  }
+}
+
+export default LoadAppScreen;
