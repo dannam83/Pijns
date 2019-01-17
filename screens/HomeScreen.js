063607d3244531d6282e0a6c873f5d9c679e00b7
Daniel Nam <dannam83@gmail.com>
2019-01-17 07:22:45 -0500

    write message is listheader

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -14,13 +14,10 @@ class HomeScreen extends Component {
 
   componentWillMount() {
     this.props.postsFetch();
-    console.log(this.props);
     this.createDataSource(this.props.posts);
   }
 
   componentWillReceiveProps(nextProps) {
-    // nextProps are next set of props to be rendered with
-    // this.props is still the old set of props
     console.log('next', nextProps);
     this.createDataSource(nextProps.posts);
   }
@@ -31,6 +28,20 @@ class HomeScreen extends Component {
     });
 
     this.dataSource = ds.cloneWithRows(posts);
+  }
+
+  renderHeader() {
+    return (
+      <View style={styles.writePostView}>
+        <Button
+          title="Write a post!"
+          onPress={() => this.props.navigation.navigate('PostCreate')}
+          backgroundColor="rgba(0,125,255,1)"
+          borderRadius={20}
+          icon={{ name: 'create' }}
+        />
+      </View>
+    );
   }
 
   renderRow(post) {
@@ -47,22 +58,16 @@ class HomeScreen extends Component {
   }
 
   render() {
-    const { writePostView } = styles;
+    const { masterContainerStyle } = styles;
 
     return (
-      <View>
-        <View style={writePostView}>
-          <Button
-            title="Write a post!"
-            onPress={() => this.props.navigation.navigate('PostCreate')}
-            backgroundColor="rgba(0,125,255,1)"
-            borderRadius={20}
-            icon={{ name: 'create' }}
-          />
-        </View>
+      <View style={masterContainerStyle}>
+
 
         <ListView
           enableEmptySections
+          renderSeperator
+          renderHeader={this.renderHeader}
           dataSource={this.dataSource}
           renderRow={this.renderRow}
         />
@@ -72,13 +77,17 @@ class HomeScreen extends Component {
 }
 
 const styles = {
+  masterContainerStyle: {
+    flex: 1,
+    backgroundColor: '#cef0ff'
+  },
   writePostView: {
-    paddingTop: 10,
-    paddingBottom: 10
+    paddingTop: 13,
   },
   containerStyle: {
     marginLeft: 0,
-    marginRight: 0
+    marginRight: 0,
+    backgroundColor: 'white'
   },
   listItemHeaderStyle: {
     marginLeft: 0,
