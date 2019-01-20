c5acf3a4edfca0533719139243c346f3e6d21cf5
Daniel Nam <dannam83@gmail.com>
2019-01-19 22:16:51 -0500

    get pijn log from firebase

--- actions/pijn_actions.js
+++ actions/pijn_actions.js
@@ -9,43 +9,41 @@ export const sendPijn = ({ postId, author, currentDate }) => {
   incrementAuthorPostPijnCount(db, author.id, postId);
   incrementPostsPijnCount(db, postId);
   firebaseRecordPijn(db, currentDate, postId);
-  asyncRecordPijn(currentDate, postId);
+  // asyncRecordPijn(currentDate, postId);
+};
+
+export const fetchPijnLog = () => {
+  const { currentUser } = firebase.auth();
+  const currentDate = new Date(
+    new Date().getFullYear(), new Date().getMonth(), new Date().getDate()
+  );
+
+  return (dispatch) => {
+    firebase.database().ref(`/users/${currentUser.uid}/pijns/${currentDate}`)
+      .on('value', snapshot => {
+        dispatch({ type: FETCH_PIJN_LOG, payload: snapshot.val() }
+        );
+      }
+    );
+  };
 };
 
 // export const fetchPijnLog = () => async dispatch => {
-//   let pijnDate = await AsyncStorage.getItem('pijn_date');
-//   console.log('date', pijnDate);
-//   const currentDate = new Date(
-//     new Date().getFullYear(), new Date().getMonth(), new Date().getDate()
-//   );
-//
-//   if (!pijnDate || pijnDate < currentDate) {
-//     await AsyncStorage.setItem('pijn_date', currentDate.toString());
-//     // await AsyncStorage.setItem('pijn_log', JSON.stringify({}));
-//   }
-//
-//   let pijnLog = await AsyncStorage.getItem('pijn_log');
-//   console.log('pijnlog', pijnLog);
-//
-//   dispatch({ type: FETCH_PIJN_LOG, payload: JSON.parse(pijnLog) });
+  // const userPijnsRef = db.ref(`/users/${uid}/pijns/${currentDate}/${postId}`);
+
+  // let pijnLog = await AsyncStorage.getItem('pijn_log');
+  //
+  // try {
+  //   if (pijnLog) {
+  //     await dispatch({
+  //       type: FETCH_PIJN_LOG, payload: JSON.parse(pijnLog)
+  //     });
+  //   }
+  // } catch (err) {
+  //   console.warn(err);
+  // }
+
 // };
-
-export const fetchPijnLog = () => async dispatch => {
-  await checkPijnDate();
-  let pijnLog = await AsyncStorage.getItem('pijn_log');
-
-  try {
-    if (pijnLog) {
-      await dispatch({
-        type: FETCH_PIJN_LOG, payload: JSON.parse(pijnLog)
-      });
-    } else {
-      await dispatch({ type: FETCH_PIJN_LOG, payload: { empty: 'nothing' } });
-    }
-  } catch (err) {
-    console.warn(err);
-  }
-};
 
 const checkPijnDate = () => async () => {
   let pijnDate = await AsyncStorage.getItem('pijn_date');
@@ -79,29 +77,23 @@ const firebaseRecordPijn = (db, currentDate, postId) => {
   userPijnsRef.set(Date.now());
 };
 
-const asyncRecordPijn = async (currentDate, postId) => {
-  let pijnLog = await AsyncStorage.getItem('pijn_log');
-  let pijnDate = await AsyncStorage.getItem('pijn_date');
-  const newPijn = JSON.stringify({ [postId]: true });
-
-  // if (pijnDate === currentDate.toString()) {
-  //   let logs = await AsyncStorage.getItem('pijn_log');
-  //   const jsonLogs = JSON.parse(logs);
-  //   console.log(jsonLogs[postId]);
-  // }
-
-  if (!pijnDate || pijnDate < currentDate) {
-    console.log('here i am');
-    await AsyncStorage.setItem('pijn_date', currentDate.toString());
-    await AsyncStorage.setItem('pijn_log', newPijn);
-    return;
-  }
-
-  let log = JSON.parse(pijnLog);
-  console.log('pijnLog before merge', log);
-  log[postId] = true;
-
-  await AsyncStorage.setItem('pijn_log', JSON.stringify(log));
-  let newLog = await AsyncStorage.getItem('pijn_log');
-  console.log('after merge and save', newLog);
-};
+// const asyncRecordPijn = async (currentDate, postId) => {
+//   let pijnLog = await AsyncStorage.getItem('pijn_log');
+//   let pijnDate = await AsyncStorage.getItem('pijn_date');
+//   const newPijn = JSON.stringify({ [postId]: true });
+//
+//   if (!pijnDate || pijnDate < currentDate) {
+//     console.log('here i am');
+//     await AsyncStorage.setItem('pijn_date', currentDate.toString());
+//     await AsyncStorage.setItem('pijn_log', newPijn);
+//     return;
+//   }
+//
+//   let log = JSON.parse(pijnLog);
+//   console.log('pijnLog before merge', log);
+//   log[postId] = true;
+//
+//   await AsyncStorage.setItem('pijn_log', JSON.stringify(log));
+//   let newLog = await AsyncStorage.getItem('pijn_log');
+//   console.log('after merge and save', newLog);
+// };
