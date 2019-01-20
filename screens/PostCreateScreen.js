327f0b6ca6b9fdd120aaf2823c6dcfb07dcc522a
Daniel Nam <dannam83@gmail.com>
2019-01-20 17:09:28 -0500

    move share button to header

--- screens/PostCreateScreen.js
+++ screens/PostCreateScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { KeyboardAvoidingView } from 'react-native';
+import { KeyboardAvoidingView, Button } from 'react-native';
 import { connect } from 'react-redux';
 
 import { postCreateSave } from '../actions';
@@ -7,11 +7,24 @@ import { CardSection, ButtonAsText } from '../components/common';
 import PostForm from '../components/post/PostForm';
 
 class PostCreateScreen extends Component {
-  static navigationOptions = {
-    title: 'Write a post',
-  };
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: 'Write a post',
+      headerRight: (
+        <Button
+          onPress={navigation.getParam('onSharePress')}
+          title="Share"
+          color="rgba(0,125,255,1)"
+        />
+      )
+    };
+  }
 
-  onButtonPress = () => {
+  componentDidMount() {
+    this.props.navigation.setParams({ onSharePress: this.onSharePress });
+  }
+
+  onSharePress = () => {
     const { postText, author } = this.props;
     this.props.postCreateSave({ postText, author });
     this.props.navigation.navigate('Home');
@@ -19,17 +32,21 @@ class PostCreateScreen extends Component {
 
   render() {
     return (
-      <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding" enabled>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior="padding"
+        enabled keyboardVerticalOffset={80}
+      >
         <PostForm {...this.props} />
-        <CardSection style={{ justifyContent: 'center', borderTopWidth: 1 }}>
-          <ButtonAsText onPress={this.onButtonPress}>
-            Share
-          </ButtonAsText>
-        </CardSection>
       </KeyboardAvoidingView>
     );
   }
 }
+// <CardSection style={{ justifyContent: 'center', borderWidth: 0 }}>
+//   <ButtonAsText onPress={this.onButtonPress}>
+//     Share
+//   </ButtonAsText>
+// </CardSection>
 
 const mapStateToProps = (state) => {
   const { postType, postText } = state.postCreate;
