bd60445fe75a5c18412f09badf8510674de0841a
Daniel Nam <dannam83@gmail.com>
2019-03-16 22:01:13 -0400

    chat fills width

--- components/chat/ChatListMessage.js
+++ components/chat/ChatListMessage.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, Dimensions } from 'react-native';
 
 import { likeComment } from '../../actions';
 import { lightTextGray } from '../../assets/colors';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
 class ChatListMessage extends Component {
   render() {
     const { containerStyle, thumbnailStyle, textViewStyle } = styles;
     const { message, userId, userPic } = this.props.message;
-    console.log(message);
 
     return (
       <View style={containerStyle}>
@@ -34,47 +35,18 @@ const styles = {
     // display: 'flex',
     flexDirection: 'row',
     // flex: 1,
-    // alignItems: 'flex-start',
+    alignItems: 'flex-start',
     justifyContent: 'space-between',
     padding: 10,
     backgroundColor: 'white',
     borderBottomWidth: 1,
-    borderColor: '#DDDDDD'
+    borderColor: '#DDDDDD',
+    width: SCREEN_WIDTH
   },
   textViewStyle: {
     flex: 1,
     paddingLeft: 5,
     paddingRight: 5
-  },
-  commentHeaderStyle: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  commentHeaderFrontStyle: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'flex-end',
-    marginBottom: 1,
-  },
-  nameStyle: {
-    fontWeight: '700',
-    fontSize: 16
-  },
-  timeAgoStyle: {
-    color: lightTextGray,
-    paddingLeft: 8,
-    marginBottom: 1.5,
-  },
-  likesStyle: {
-    paddingTop: 0.5,
-    fontStyle: 'italic',
-    color: '#808080'
-  },
-  commentStyle: {
-    color: '#000',
-    fontSize: 16,
-    lineHeight: 20,
   },
   thumbnailStyle: {
     height: 47,
@@ -82,24 +54,6 @@ const styles = {
     borderRadius: 25,
     marginRight: 8,
     marginLeft: 8,
-  },
-  buttonStyle: {
-    height: 47,
-    width: 22,
-    paddingTop: 0.5,
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'flex-start',
-  },
-  iconNotLikedStyle: {
-    height: 17,
-    width: 17,
-    tintColor: '#D3D3D3'
-  },
-  iconLikedStyle: {
-    height: 17,
-    width: 17,
-    tintColor: '#FF0000'
   },
 };
 
