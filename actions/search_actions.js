ef7e37465fb34fcbc499641572c298faf4ef668c
Daniel Nam <dannam83@gmail.com>
2019-08-10 09:13:21 -0400

    search return empty when no input

--- actions/search_actions.js
+++ actions/search_actions.js
@@ -4,14 +4,17 @@ import { SEARCH_UPDATE } from './types';
 
 export const searchUpdate = ({ value }) => {
   return (dispatch) => {
-    firebase.database().ref('/userSearch')
+    if (value === '') {
+      dispatch({ type: SEARCH_UPDATE, payload: {} });
+    } else {
+      firebase.database().ref('/userSearch')
       .orderByChild('searchName')
       .startAt(value)
       .endAt(`${value}\uf8ff`)
       .limitToFirst(50)
       .once('value', snapshot => {
         dispatch({ type: SEARCH_UPDATE, payload: snapshot.val() });
-      }
-    );
+      });
+    }
   };
 };
