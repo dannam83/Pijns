4dd6deda0ed5f00ef2693012a9320b053a8e9691
Daniel Nam <dannam83@gmail.com>
2019-08-26 21:04:41 -0400

    save pushToken to firebase

--- services/push_notifications.js
+++ services/push_notifications.js
@@ -3,7 +3,9 @@ import * as Permissions from 'expo-permissions';
 import Constants from 'expo-constants';
 import { AsyncStorage } from 'react-native';
 
-export default async () => {
+import { savePushToken } from '../api/pushNotifications';
+
+export default async (userId) => {
   if (Constants.isDevice) {
     let previousToken = await AsyncStorage.getItem('pushToken');
     if (previousToken) { return; }
@@ -12,24 +14,8 @@ export default async () => {
     if (status !== 'granted') { return; }
 
     let token = await Notifications.getExpoPushTokenAsync();
+    savePushToken(userId, token);
     AsyncStorage.setItem('push_token', token);
-
-  //   const { status: existingStatus } = await Permissions.getAsync(
-  //     Permissions.NOTIFICATIONS
-  //   );
-  //   let finalStatus = existingStatus;
-  //   if (existingStatus !== 'granted') {
-  //     const { status } = await Permissions.askAsync(
-  //       Permissions.NOTIFICATIONS
-  //     );
-  //     finalStatus = status;
-  //   }
-  //   if (finalStatus !== 'granted') {
-  //     alert('Failed to get push token for push notification!');
-  //     return;
-  //   }
-  //   let token = await Notifications.getExpoPushTokenAsync();
-  //   console.log(token);
   } else {
     console.warn('Must use physical device for Push Notifications');
   }
