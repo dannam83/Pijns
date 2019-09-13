73093ff98a81b6de311e588064db8e4c961534a9
Daniel Nam <dannam83@gmail.com>
2019-09-12 22:03:15 -0400

    update expo to v34.0.3

---
+++ utilities/cacheAssetsAsync.js
@@ -0,0 +1,21 @@
+import { Image } from 'react-native';
+import { Asset } from 'expo-asset';
+import * as Font from 'expo-font';
+
+export default function cacheAssetsAsync({ images = [], fonts = [] }) {
+  return Promise.all([...cacheImages(images), ...cacheFonts(fonts)]);
+}
+
+function cacheImages(images) {
+  return images.map(image => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image);
+    }
+
+  return Asset.fromModule(image).downloadAsync();
+  });
+}
+
+function cacheFonts(fonts) {
+  return fonts.map(font => Font.loadAsync(font));
+}
