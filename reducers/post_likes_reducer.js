0543bceda13d83ff21d4e141d5c5d8f99b8a1b68
Daniel Nam <dannam83@gmail.com>
2019-11-20 21:09:41 -0500

    fix bug with likes

--- reducers/post_likes_reducer.js
+++ reducers/post_likes_reducer.js
@@ -5,7 +5,7 @@ const INITIAL_STATE = {};
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case FETCH_POST_LIKES:
-      return action.payload ? action.payload : state;
+      return action.payload || INITIAL_STATE;
     default:
       return state;
   }
